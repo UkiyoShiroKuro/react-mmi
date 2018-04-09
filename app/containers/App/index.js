396c20b0efb3974298dd7650ccc8ac84a75e851d
Elody Chaumarat <elody.chaumarat@etu.univ-grenoble-alpes.fr>
2018-04-09 08:08:39 +0000

    ARG TOUT MODIFIER3

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -17,7 +17,7 @@ import { BrowserRouter, Link, Route } from 'react-router-dom';
 // On peut aussi importer du CSS de la meme facon.
 import CSS from '../../css/style.css';
 
-
+/*
 class App extends React.Component{
   constructor(props){
     super(props);
@@ -140,7 +140,79 @@ class App extends React.Component{
       <div style= {{padding: '20px'}} {...props}/>
     </div>
     )
+  */
   
+  class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleStatusChange = this.handleStatusChange.bind(this);
+
+    this.state = {
+      machines: [
+        {
+          id: 0,
+          name: "Machine à café",
+          isActive: true
+        },
+        {
+          id: 1,
+          name: "Machine à thé",
+          isActive: false
+        },
+        {
+          id: 2,
+          name: "Machine à frappucino",
+          isActive: true
+        },
+        {
+          id: 3,
+          name: "Machine à citron",
+          isActive: true
+        }
+      ]
+    };
+  }
+
+  // Méthode pour activer une machine
+  handleStatusChange(key) {
+    // 1. On copie le state existant
+    const machines = { ...this.state.machines };
+    // 2. On modifie le status de CETTE machine
+    machines[key].isActive = true;
+    // Pour vérifier la nouvelle collection dans la console :
+    console.log({ machines });
+
+    // 3. On applique cette nouvelle collection au state
+    this.setState({ machines });
+  }
+
+  render() {
+    return (
+      <div className="main">
+        <Header/>
+          {/*Conteneur de notre liste*/}
+          <div className="machines-list">
+            {/*Boucle sur notre collection de machines*/}
+            {
+              Object
+                .keys(this.state.machines)
+                .map(key =>
+                // Le composant Machine s'affichera autant de fois
+                // qu'il y a d'objets dans la collection.
+                <Machine name={this.state.machines[key].name}
+                         key={this.state.machines[key].id}
+                         index={this.state.machines[key].id}
+                         handleStatusChange={this.handleStatusChange}
+                         isActive={this.state.machines[key].isActive}/>
+              )}
+          </div>
+        <Footer/>
+      </div>
+    );
+  }
+}
+
 export default App;
 
 /*Return initial
