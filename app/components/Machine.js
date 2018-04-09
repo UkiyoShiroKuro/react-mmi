c2f7fe22502595fc3166af83555bf0624892dad8
Elody Chaumarat <elody.chaumarat@etu.univ-grenoble-alpes.fr>
2018-04-09 08:00:31 +0000

    ARG TOUT MODIFIER2

--- app/components/Machine.js
+++ app/components/Machine.js
@@ -5,7 +5,7 @@
  */
 import React from 'react';
 
-class Machine extends React.Component {
+/*class Machine extends React.Component {
   onToggleClick() {
     console.log('Clic sur le bouton ------- ');	+    // On envoie l'index à la méthode qui changera la valeur
     // On accède dans cette méthode à ce qu'on a passé en props au composant	+    // La méthode et son argument sont tous les deux accessibles
@@ -28,7 +28,30 @@ class Machine extends React.Component {
           </div>
         </div>
       </div>
-    );*/
+    );
+  }
+}*/
+
+class Machine extends React.Component {
+  // Méthode lancée au clic sur le bouton "Toggle"
+  onToggleClick() {
+    // On envoie l'index à la méthode qui changera la valeur
+    // La méthode et son argument sont tous les deux accessibles
+    // via des props qui ont été passées au composant
+    this.props.handleStatusChange(this.props.index);
+  }
+
+  render() {
+    return (
+        <div
+        className= { this.props.isActive ? "machine active" : "machine" }> {/* Si isActive passée en props est à true, ajouter la classe "active" */}
+          {this.props.name}
+          {/* On appelle handleClick avec (e) pour pouvoir accéder à this*/}
+          <button onClick={(e) => this.onToggleClick(e)} type="button" className="btn">
+            Activer
+          </button>
+        </div>
+      )
   }
 }
 
