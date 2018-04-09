d7d8b9b53efdf2d5235d8a208abc3f42f36eb817
Elody Chaumarat <elody.chaumarat@etu.univ-grenoble-alpes.fr>
2018-04-09 07:34:59 +0000

    Machine modif3

--- app/components/Machine.js
+++ app/components/Machine.js
@@ -14,7 +14,7 @@ class Machine extends React.Component {
           <div className= { this.props.isActive ? "machine active" : "machine" }>
             <h2>{this.props.name} La vie se résume à ça : </h2>
           </div>
-          <p className="text">{this.props.desc}</p> | {this.props.isActive}
+          <p className="text">{this.props.desc}</p>
           <div className="Machine-footer footer">
             <p>I don't care I SHIP IT ! Why not ?</p>
           </div>
